92fd172eb9cc03cab82852fb1eed96aa8d240a47
Isaac Pachon <isaac.pachon@utp.edu.co>
2025-02-24 08:48:18 -0500

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Isaac Pachon on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
