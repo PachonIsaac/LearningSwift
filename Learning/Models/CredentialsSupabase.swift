f4ba3cc95f09a4111e4d2b4a492bd8756b0dab0e
Isaac Pachon <isaac.pachon@utp.edu.co>
2025-03-13 12:25:39 -0500

    Feat: Connection with Supabase

---
+++ Learning/Models/CredentialsSupabase.swift
@@ -0,0 +1,13 @@
+//
+//  CredentialsApp.swift
+//  Learning
+//
+//  Created by Isaac Pachon on 13/03/25.
+//
+
+import Foundation
+
+struct CredentialsApp {
+    static let supabaseUrl = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_URL") as? String ?? ""
+    static let supabaseKey = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_KEY") as? String ?? ""
+}
--- Learning/ViewsModels/AuthViewModel.swift
+++ Learning/ViewsModels/AuthViewModel.swift
@@ -5,19 +5,57 @@
 //  Created by Isaac Pachon on 4/03/25.
 //
 
-import SwiftUI
+import Foundation
+import Supabase
 
-class AuthViewModel: ObservableObject {
+class LoginViewModel: ObservableObject {
     @Published var isAuthenticated = false
     @Published var errorMessage: String? = nil
+    @Published var userRole: String? = nil
     
+    private let client = SupabaseClient(
+        supabaseURL: URL(string: "https://buyvwbxhgakyjynjvxku.supabase.co")!,
+        supabaseKey: CredentialsSupabase.supabaseKey
+    )
     
-    func login(username: String, password: String) {
-        if username == "admin" && password == "admin"{
-            isAuthenticated = true
-        } else {
-            errorMessage = "Wrong Credentials"
+    func login(email: String, password: String) async {
+        do {
+            // Intentar iniciar sesión con Supabase Auth
+            let authResponse = try await client.auth.signIn(email: email, password: password)
+            
+            // Obtener el usuario autenticado
+            guard let session = authResponse.session else {
+                DispatchQueue.main.async {
+                    self.errorMessage = "No se pudo obtener la sesión del usuario."
+                }
+                return
+            }
+            
+            // Buscar en la base de datos el usuario con ese correo
+            let users: [User] = try await client.database
+                .from("USERS")
+                .select()
+                .eq("USER_MAIL", email)
+                .limit(1)
+                .execute()
+                .decoded()
+            
+            guard let user = users.first else {
+                DispatchQueue.main.async {
+                    self.errorMessage = "Usuario no encontrado en la base de datos."
+                }
+                return
+            }
+            
+            // Guardamos el rol del usuario
+            DispatchQueue.main.async {
+                self.userRole = user.role
+                self.isAuthenticated = true
+            }
+        } catch {
+            DispatchQueue.main.async {
+                self.errorMessage = "Error de autenticación: \(error.localizedDescription)"
+            }
         }
     }
-    
 }
